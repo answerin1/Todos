d0b183f123d3ef14c4fa08417de69b5010a6503e
answerin1 <dabin990723@naver.com>
2025-01-06 12:43:03 +0900

    fix: comment 관련 기능 추가 (미완성)

---
+++ src/main/java/com/example/todo/dto/UpdateCommentRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.todo.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UpdateCommentRequestDto {
+
+  // 속성
+  private final String content;
+}
+
+// 생성자
+
+// 기능
